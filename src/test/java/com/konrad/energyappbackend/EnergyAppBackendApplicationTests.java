e6aea025c6bb21623f00f428ffee4eedc26fa5ab
Konrad Małek <konrad.malek1@gmail.com>
2025-12-01 12:53:30 +0100

    init commit

---
+++ src/test/java/com/konrad/energyappbackend/EnergyAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konrad.energyappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
